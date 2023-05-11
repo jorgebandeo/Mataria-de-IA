4cf1f34a353b16796a2f2e644c3399db8d6cbfc9
Jorge Bandeo <jorgebandeo+Github@hotmail.com>
2023-05-11 12:21:48 -0300

    Knn na mao tem alteração para a construção dinamica do dataset trabalhado, implementando os tres tipos principais de similaridade, e implementando os pesos de cada elemento

--- KNN_na_mao.py
+++ KNN_na_mao.py
@@ -1,9 +1,133 @@
 import pandas as pd
 import csv
+#pesos 
+pesos = {
+    "school":1,
+    "sex":1,
+    "age":1,
+    "address":1,
+    "famsize":1,
+    "Pstatus":1,
+    "Medu":1,
+    "Fedu":1,
+    "Mjob":1,
+    "Fjob":1,
+    "reason":1,
+    "guardian":1,
+    "traveltime":1,
+    "studytime":1,
+    "failures":1,
+    "schoolsup":1,
+    "famsup":1,
+    "paid":1,
+    "activities":1,
+    "nursery":1,
+    "higher":1,
+    "internet":1,
+    "romantic":1,
+    "famrel":1,
+    "freetime":1,
+    "goout":1,
+    "Dalc":1,
+    "Walc":1,
+    "health":1,
+    "absences":1,
+    "G1":1,
+    "G2":1,
+    "G3":1
+}
+TS = {#similaridade 1 numerico 2 binario 3 matrizial
+    "school":1,
+    "sex":1,
+    "age":1,
+    "address":1,
+    "famsize":1,
+    "Pstatus":1,
+    "Medu":1,
+    "Fedu":1,
+    "Mjob":1,
+    "Fjob":1,
+    "reason":1,
+    "guardian":1,
+    "traveltime":1,
+    "studytime":1,
+    "failures":1,
+    "schoolsup":1,
+    "famsup":1,
+    "paid":1,
+    "activities":1,
+    "nursery":1,
+    "higher":1,
+    "internet":1,
+    "romantic":1,
+    "famrel":1,
+    "freetime":1,
+    "goout":1,
+    "Dalc":1,
+    "Walc":1,
+    "health":1,
+    "absences":1,
+    "G1":1,
+    "G2":1,
+    "G3":1
+}
 # Carregar o conjunto de dados a partir do arquivo CSV
+vetor_de_matrizes ={'Mjob' : [['Mjob'     ,'at_home'     , 'health'  , 'other'   , 'services', 'teacher'],
+                      ['at_home'  ,1     , 0.2   , 0.5  , 0.2 , 0.2],
+                      ['health'   ,None  , 1     , 0.5  , 0.4 , 0.6],
+                      ['other'    ,None  , None  , 1    , 0,5 , 0,5],
+                      ['services' ,None  , None  , None , 1   , 0.7],
+                      ['teacher'  ,None  , None  , None , None, 1  ]],
+                      'Fjob': [['Fjob'     ,'at_home'     , 'health'  , 'other'   , 'services', 'teacher'],
+                      ['at_home'  ,1     , 0.2   , 0.5  , 0.2 , 0.2],
+                      ['health'   ,None  , 1     , 0.5  , 0.4 , 0.6],
+                      ['other'    ,None  , None  , 1    , 0,5 , 0,5],
+                      ['services' ,None  , None  , None , 1   , 0.7],
+                      ['teacher'  ,None  , None  , None , None, 1  ]]
+                      } #vetor contem as matrizes de semelhança
+# similaridade matematica 
+
 def similaridade(F1,F2):
     a = F1 - F2 if F1 - F2 >=0 else F2 - F1
     return 1-(a/100)
+def similaridadeMatrizial(F1,F2, nome ):
+    matriz = vetor_de_matrizes[nome]
+    coluna = 0
+    linha = 0
+    for i in range(len(matriz)):
+        if  matriz[0][i] == F1:
+            coluna = i
+    for i in range(len(matriz)):
+        if  matriz[i][0] == F2:
+            linha = i
+    return matriz[linha][coluna]
+
+def similaridadeBinaria(F1,F2):
+    if F1 == F2:
+        return 1.0
+    else:
+        return 0.0
+
+
+def matriz_de_similaridade(Matriz, entrada, P, tipo_de_similaridade):
+    Matriz[0].append("similaridade")#adiciona a coluna de similaridade
+    inferior = 0
+    for i in range(len(P)-1):#soma dos pessoas de cada coluna
+        inferior = inferior + P[Matriz[0][i]]
+    
+    for i in range(1,len(matriz)):#linhas do dataset
+        sup = 0
+        for j in range(len(matriz[i])-1):#colunas da linha do dataset
+            ax = Matriz[0][i]
+            if tipo_de_similaridade[ax] == 1:
+                sup += P[ax] * similaridade(int(matriz[i][j]), entrada[j])
+            elif tipo_de_similaridade[ax] == 2:
+                sup += P[ax] * similaridadeBinaria(matriz[i][j], entrada[j])
+            else:
+                sup += P[ax] * similaridadeMatrizial(matriz[i][j], entrada[j],Matriz[0][j])
+        Matriz[i].append(sup/inferior)
+    return Matriz
+            
 def ditancia(datatrein, novo, pesos):
     vetorD = []
     inf = 0 
@@ -16,11 +140,21 @@ def ditancia(datatrein, novo, pesos):
         vetorD.append([sup/inf, datatrein[i][-1]])
     vetorD = sorted(vetorD, key=lambda x: x[0])   
     return vetorD
-matriz = []
-with open('novo csv', newline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-    for linha in reader:
-        matriz.append(linha)
-novo = ditancia(matriz,[1,1,2,0,1,0,1],[1,1,1,1,1,1,1])
-for i  in novo:
-    print(i)
+if __name__ == "__main__":
+    Teste = []
+    matriz = []
+    # Abre e transforma em matriz o dataset de testes 
+    with open('Trabalho M2\datasetTestes.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for linha in reader:
+            Teste.append(linha)
+    # Abre e transforma em matriz o dataset de trinamento
+    with open('Trabalho M2\student-mat.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for linha in reader:
+            matriz.append(linha)
+    # Chama função que a aliza similaridade de uma variavel
+    novo = matriz_de_similaridade(matriz,Teste[0],pesos, TS)
+    # prinata matris com a similaridade de cada elemento com realação o objeto 
+    for i  in novo:
+        print(i)
--- Trabalho M2/KNN.py
+++ Trabalho M2/KNN.py
@@ -1,42 +1,18 @@
 import pandas as pd
-def recot(df,a, nome):
-    for j in range (len(a)):
-        for i in range (df[nome].shape[0]):
-            if df[nome][i] == a[j]:
-                df[nome][i] = j
-    return df 
-def nota(notas, df, nome ):
-    for i in range((df["gender"].shape[0])):
-        var = int(df[nome][i])
-        if var <= 100 and var>= 90: 
-            df[nome][i] = notas[0]
-        elif var < 90 and var>= 85:
-            df[nome][i] = notas[1]
-        elif var < 85 and var>= 60:
-            df[nome][i] = notas[2]
-        elif var < 60 and var>= 40:
-            df[nome][i] = notas[3]
-        elif var < 40 and var>= 0:
-            df[nome][i] = notas[4]
-    return df
+import csv
 
+matriz = []
+df = pd.read_csv('Trabalho M2\student-mat.csv')
 
-df = pd.read_csv('Trabalho M2/exams.csv')
-sexos = df["gender"].unique()
-estnia = df["race/ethnicity"].unique()
-ple = df["parental level of education"].unique()
-lunch = df["lunch"].unique()
-tpc = df["test preparation course"].unique()
-df = recot(df, sexos,"gender")
-df = recot(df, estnia,"race/ethnicity")
-df = recot(df, ple,"parental level of education")
-df = recot(df, lunch,"lunch" )
-df = recot(df, tpc,"test preparation course" )
-notas = [3,2,1, 0, 4]
-
-df = nota(notas, df,"math score")
-df = nota(notas, df,"reading score")
-df = nota(notas, df,"writing score")
-
-df.to_csv("novo csv", index=False)
+with open('Trabalho M2\student-mat.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    for linha in reader:
+        matriz.append(linha)
+valores_das_colunas = []
+for i in matriz[0]:
+    valores_das_colunas.append(df[i].unique()) 
+for i in range(len(matriz[0])):
+    print(matriz[0][i], " ___ ", valores_das_colunas[i])
     
+
+
